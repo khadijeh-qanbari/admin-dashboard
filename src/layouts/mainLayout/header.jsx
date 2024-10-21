c9d28df6472dfea6565ee0ee703f99d4cc8acdb5
khadijeh-qanbari <khadijeqanbari.prog@gmail.com>
2024-10-21 18:49:38 +0330

    add logout

--- src/layouts/mainLayout/header.jsx
+++ src/layouts/mainLayout/header.jsx
@@ -3,9 +3,15 @@ import {useAppContext} from "../../contexts/app/app-context.jsx";
 import ChangeTheme from "../../components/theme/change-theme.jsx";
 import ChangeLanguage from "../../components/language/change-language.jsx";
 import {FiMenu} from "react-icons/fi";
+import {useNavigate} from "react-router-dom";
 
 const Header = () => {
     const {changeSidebar} = useAppContext();
+    const navigate = useNavigate();
+    const logOut = () => {
+        localStorage.removeItem("token");
+        navigate("/login");
+    }
     return (
         <div className="header-nav d-flex justify-content-between shadow-sm">
             <div className="navbar  d-flex justify-content-start align-items-center gap-3">
@@ -15,7 +21,7 @@ const Header = () => {
                 <ChangeLanguage/>
                 <ChangeTheme/>
             </div>
-            <button type="button" className="btn btn-outline-danger my-3 mx-2">
+            <button type="button" className="btn btn-outline-danger my-3 mx-2" onClick={logOut}>
                 خارج شوید
             </button>
         </div>
